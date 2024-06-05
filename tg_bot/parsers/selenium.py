e8ff1976d69875d60b09af627d47649b702f2e1c
egorkjj <d7085195@gmail.com>
2024-06-05 23:30:53 +0300

    init

--- tg_bot/parsers/selenium.py
+++ tg_bot/parsers/selenium.py
@@ -1,12 +1,14 @@
 import undetected_chromedriver as uc
 from undetected_chromedriver import By
-
+from pyvirtualdisplay import Display
 driver = None
 
 def run_selenium(article):
     global driver
     def login():
         global driver
+        display = Display(visible=0, size=(1024, 768))
+        display.start()
         driver = uc.Chrome()
         driver.get(f'https://ozon.ru/context/detail/id/{article}/')
         while f"https://ozon.ru/context/detail/id/{article}/" in driver.current_url:
@@ -16,5 +18,6 @@ def run_selenium(article):
         curr = previous_sibling.find_element(By.CSS_SELECTOR, "*")
         txt = curr.text
         driver.quit()
+        display.stop()
         return txt
     return login()
